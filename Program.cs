f787fe801f5eada946991ff4ead53d60b0639a94
Ahmed Hamdy <ahmedhamdy20@gmail.com>
2020-07-13 01:39:35 +0200

    Complete solve the problem: the worker services logic using Facade design pattern

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using FacadePattern.Entities;
-using FacadePattern.Services;
 
 namespace FacadePattern
 {
@@ -8,25 +6,14 @@ namespace FacadePattern
     {
         static void Main(string[] args)
         {
-            // As see this problem of multi-dependencies to apply one operation
-            // get the current temperature for given location by zip code value
             const string zipCode = "98074"; 
                 
-            // call service 1
-            GeoLookupService geoLookupService = new GeoLookupService();
-            City city = geoLookupService.GetCityForZipCode(zipCode);
-            State state = geoLookupService.GetStateForZipCode(zipCode);
-            
-            // call service 2
-            WeatherService weatherService = new WeatherService();
-            int fahrenheit = weatherService.GetTempFahrenheit(city, state);
-            
-            // call service 3
-            ConverterService metricConverter = new ConverterService();
-            int celsius = metricConverter.ConvertFahrenheitToCelsius(fahrenheit);
+            WeatherFacade weatherFacade = new WeatherFacade();
+
+            WeatherFacadeResults results = weatherFacade.GetTempInCity(zipCode);
             
             Console.WriteLine("The current temperature is {0} F / {1} C in {2}, {3}",
-                fahrenheit, celsius, city.Name, state.Name);
+                results.Fahrenheit, results.Celsius, results.City.Name, results.State.Name);
         }
     }
 }
